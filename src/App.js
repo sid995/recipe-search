cce202bf4b4b0e193f4fd3d86c168ec8bed747ef
Siddharth Kundu <sk31395@gmail.com>
2019-04-15 02:30:27 +0530

    Added temporary mock data and passed recipe prop to Recipe

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-/* eslint-disable no-undef */
 /* eslint-disable react/no-unused-state */
 import React, { Component } from 'react';
 import './App.css';
 import RecipeDetails from './components/RecipeDetails';
-// import { recipes } from './tempList';
 import RecipeList from './components/RecipeList';
+import { recipeList } from './tempList';
 
 class App extends Component {
   state = {
-    recipes: [],
+    recipes: recipeList,
     url: 'https://www.food2fork.com/api/search?key=1642fcc46a1921e19df4e20e7deec490',
   };
 
-  componentDidMount() {
-    this.getRecipes();
-  }
+  // componentDidMount() {
+  //   this.getRecipes();
+  // }
 
-  async getRecipes() {
-    try {
-      const { url } = this.state;
-      const data = await fetch(url);
-      const jsonData = await data.json();
-      this.setState({
-        recipes: jsonData.recipes,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
+  // async getRecipes() {
+  //   try {
+  //     const { url } = this.state;
+  //     const data = await fetch(url);
+  //     const jsonData = await data.json();
+  //     this.setState({
+  //       recipes: jsonData.recipes,
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
 
   render() {
     const { recipes } = this.state;
-    console.log(recipes);
     return (
       <>
-        <RecipeList />
+        <RecipeList recipes={recipes} />
         <RecipeDetails />
       </>
     );
